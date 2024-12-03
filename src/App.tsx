d40e0fce9db59b2fd7b542bf2bf03f47c4cecfe5
Artemii <artemii.fedotov@tutamail.com>
2024-12-03 20:41:59 +0100

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -60,8 +60,8 @@ const App: Component = () => {
         { name: "Rust" },
         { name: "Bun" },
         { name: "TypeScript" },
-        { name: "Backend" },
         { name: "Docker" },
+        { name: "Backend" }
       ],
     },
   ];
